e0d1fec3c9cd7d402da1a1f2ee3037f4fa4ee160
duferdias <35440509+duferdias@users.noreply.github.com>
2018-01-24 20:57:47 -0300

    Stratum Proxy Installer
    Instala e configura um stratum-proxy em ambiente Linux gerando um Servidor Proxy para adição de mineradores de bitcoin apontando para a Zpool.
    O proxy pode ser reconfigurado pelo navegador após acesso com usuário e senha.
    O proxy suporta até 256 mineiros e os algorítimos Yescrypt, Scrypt e SHA256.
    Comando para instalar: wget -O install-proxy.sh https://git.io/vNVRy && sh install-proxy.sh

--- install-proxy.sh
+++ install-proxy.sh
@@ -2,7 +2,7 @@
 # by Darkstilo & 8TH
 # Editado por duferdias
 
-# [ PROXY STRATUM METHOD ]
+# [ PROXY STRATUM METHOD FOR LINUX ]
 
 # [ VARIABLES ]
 carteira=
@@ -26,16 +26,19 @@ if [ -z "${senha-}" ]; then
   read -p "[read] Entre com a senha do usuário: " senha
 fi
 
-# [ JDK Install ] 
-sudo apt-get update
-sudo apt-get install default-jre
-sudo apt-get install default-jdk
+# [ Install JDK, Extras, Packages and Dependencies ]
+sudo apt-get update -y
+sudo apt-get install screen -y
+sudo apt-get install gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget libxext6 -y
+sudo apt-get install build-essential libcurl4-openssl-dev gcc make git nano autoconf automake -y
+sudo apt-get install autotools-dev -y
+sudo apt-get install libcurl3 libcurl4-gnutls-dev -y
+sudo apt-get install default-jre -y
+sudo apt-get install default-jdk -y
+sudo apt-get install nodejs -y
+sudo apt-get install maven -y
 
-# [ Extras ]
-sudo apt-get install nodejs
-sudo apt-get install maven
-
-# [ Stratum Proxy ]
+# [ Install Stratum Proxy ]
 wget https://github.com/darkstilo/stratum-proxy/archive/v0.8.1.1-malthraxcrypto.tar.gz -O v0.8.1.1-malthraxcrypto.tar.gz
 tar -xzvf v0.8.1.1-malthraxcrypto.tar.gz
 cd stratum-proxy-0.8.1.1-malthraxcrypto
@@ -44,7 +47,7 @@ cd target
 
 wget https://github.com/darkstilo/stratum-proxy/releases/download/v0.8.1.1-malthraxcrypto/stratum-proxy-yescrypt.conf -O stratum-proxy-yescrypt.conf
 
-# Convert formato dos2unix
+# Convert format dos2unix
 # awk '{printf "%s\r\n", $0}' stratum-proxy-yescrypt.conf
 # Insere as variáveis
 sed -i 's/XMY/BTC/g' stratum-proxy-yescrypt.conf
@@ -52,16 +55,23 @@ sed -i "s/\"user\" :.*/\"user\" \: \"${carteira}\"\,/" stratum-proxy-yescrypt.co
 sed -i "s/\"apiUser\":.*/\"apiUser\"\: \"${usuario}\"\,/" stratum-proxy-yescrypt.conf
 sed -i "s/\"apiPassword\":.*/\"apiPassword\"\: \"${senha}\"\,/" stratum-proxy-yescrypt.conf
 
-# Executa o proxy
-sleep 1
-
-echo "\033[01; 41m Inicializando Stratum Proxy... \033[0m \n "
-sleep 1
-echo "\033[01;31m STRATUM PROXY INICIA SCREEN AUTOMÁTICO... "
+echo "\033[01;32mInstalação e configuração concluídas com sucesso."
 sleep 3
 clear
-echo "\033[37;41m Stratum Proxy foi iniciado... Abrindo sessão em 3 segundos\033[01;0m \n"
-echo "\033[37;41m COMANDO [screen -x sproxy] \033[0m"
+
+echo "\033[44;1;37m      O Stratum Proxy iniciará em uma nova sessão.      \033[0m "
+echo "\033[44;1;37m          NÃO FECHE A JANELA DO CONSOLE AGORA!          \033[0m "
+echo "\033[44;1;37m      Fechar console somente após a inicialização.      \033[0m "
+sleep 10
+clear
+
+# [ Run Stratum Proxy ]
+echo "\033[37;41mAbrindo sessão, aguarde...\033[0m "
 sleep 3
 sudo screen -dmS sproxy java -jar stratum-proxy-0.8.1-malthraxcrypto.jar --conf-file=stratum-proxy-yescrypt.conf
+sleep 2
+clear
+
+echo "\033[37;41mStratum Proxy em execução. Para visualizar digite o comando abaixo:\033[01;0m \n"
+echo "\033[37;41mCOMANDO [screen -x sproxy]\033[0m"
 sleep 1
