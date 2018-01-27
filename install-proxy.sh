4cf6f4638ccf5e4dd556f586a511651d0f3e0dec
duferdias <35440509+duferdias@users.noreply.github.com>
2018-01-27 00:11:26 -0300

    Stratum Proxy Installer
    Instala e configura um stratum-proxy em ambiente Linux gerando um Servidor Proxy para adição de mineradores de bitcoin apontando para a Zpool.
    O proxy pode ser reconfigurado pelo navegador após acesso com usuário e senha.
    O proxy suporta até 256 mineiros e os algorítimos Yescrypt, Scrypt e SHA256.
    Comando para instalar: wget -O install-proxy.sh https://git.io/vNVRy && sh install-proxy.sh

--- install-proxy.sh
+++ install-proxy.sh
@@ -29,8 +29,8 @@ fi
 # [ Install JDK, Extras, Packages and Dependencies ]
 sudo apt-get update
 sudo apt-get install screen -y
-sudo apt-get install gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget libxext6 -y
-sudo apt-get install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev gcc make git nano autoconf automake -y
+sudo apt-get install gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils libxext6 -y
+sudo apt-get install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev nano -y
 sudo apt-get install autotools-dev -y
 sudo apt-get install libcurl3 libcurl4-gnutls-dev -y
 sudo apt-get install software-properties-common -y
