7020ed345a52bd2ef204d64d388a5ddef0aa331a
duferdias <35440509+duferdias@users.noreply.github.com>
2018-02-06 02:02:48 -0300

    Stratum Proxy Yescryptr16 - Pool [ytn.misosi.ru]

--- sproxy-ytn.sh
+++ sproxy-ytn.sh
@@ -15,6 +15,22 @@ clear
 # para receber mineradores de YENTEN e redirecioná-los para a ytn.misosi.ru.
 
 # -------------
+echo "\033[44;1;37mLiberando portas no firewall iptables...aguarde...\033[0m"
+sleep 5
+sudo iptables -A INPUT -p tcp --dport 3333 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 3433 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 6233 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 8233 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 8888 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 16011 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 16012 -j ACCEPT
+sleep 1
+clear
+echo "\033[44;1;37mPortas liberadas com sucesso.\033[0m"
+sleep 5
+clear
+
+# -------------
 # [ VARIABLES ]
 WLogin_WName=
 passwordMiner=
