9df199b3c0a5c29be9bf4cb731bf3b70708a00df
duferdias <35440509+duferdias@users.noreply.github.com>
2018-08-03 13:56:40 -0300

    Script para limpeza de cache em loop

--- clearmc.sh
+++ clearmc.sh
@@ -5,10 +5,10 @@ while : ; do
   if [[ ! -e /tmp/lockfile ]]; then
     touch /tmp/lockfile
     {
-      sync; echo 3 > /proc/sys/vm/drop_caches
-      # sysctl -w vm.drop_caches=3
+      # sync; echo 3 > /proc/sys/vm/drop_caches
+      sysctl -w vm.drop_caches=3
       rm /tmp/lockfile
     } & # Note o "&" para execução em background.
   fi
-  sleep 300
+  sleep 180
 done
